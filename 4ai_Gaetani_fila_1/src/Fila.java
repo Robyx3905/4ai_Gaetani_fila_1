445fc3187f98e8637905aa463c779bd00ca37fc1
LENOVO <LENOVO@LENOVO>
2023-01-21 12:03:23 +0100

    Venneri

--- 4ai_Gaetani_fila_1/src/Fila.java
+++ 4ai_Gaetani_fila_1/src/Fila.java
@@ -2,6 +2,7 @@
 public class Fila {
 		public static void main(String[] args) {
 			System.out.println("Gaetani Roberto");
-			System.out.println("Olimpio Matteo")	;	
+			System.out.println("Olimpio Matteo")	;
+			System.out.println("Ariano Venneri");
 			}
 }
